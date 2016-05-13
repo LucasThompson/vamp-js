c17b54dc02f459a4661deb6dd261a5bcfd3c1a10
Lucas Thompson <dev@lucas.im>
2016-05-13 15:37:06 +0800

    Initial Commit

---
+++ basic-vamp-host/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basic-vamp-host
+//
+//  Created by Lucas Thompson on 13/05/2016.
+//  Copyright © 2016 LTDSP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
